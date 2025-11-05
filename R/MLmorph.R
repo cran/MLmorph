b34c8d1eb2c802b2cbacf584762f701f76506ce7
Oskar Kosch <contact@oskarkosch.com>
2025-11-05 20:40:02 +0000

    version 0.1.1

--- R/MLmorph.R
+++ R/MLmorph.R
@@ -18,6 +18,7 @@ MLmorph <- function(host = "127.0.0.1",
                     port = NULL,
                     launch.browser = TRUE,
                     maxUploadSize = 200 * 1024^2){
+    MLmorph_dependencies()
     options(shiny.maxRequestSize = maxUploadSize)
     shiny::shinyOptions(maxUploadSize = maxUploadSize)
     shiny::runApp(system.file("MLmorph",
@@ -32,7 +33,88 @@ MLmorph <- function(host = "127.0.0.1",
 #'
 #' @keywords internal
 MLmorph_live <- function() {
+    MLmorph_dependencies()
     options(shiny.maxRequestSize = 200 * 1024^2)
     shiny::shinyOptions(maxUploadSize = 200 * 1024^2)
     shiny::runApp(paste0(getwd(),"/inst/MLmorph"), launch.browser = TRUE, host = "127.0.0.1")
+}
+
+#' Iterate through app libraries and functions
+#'
+#' This function references packages and functions used in the MLmorph Shiny app,
+#' located in `inst/`.
+#'
+#' @keywords internal
+#' @return NULL
+#' @export
+MLmorph_dependencies <- function() {
+    # Shiny
+    shiny::fluidPage
+    shiny::navbarPage
+    shiny::fileInput
+    shiny::actionButton
+    shiny::selectizeInput
+    shiny::sliderInput
+    shiny::checkboxInput
+    shiny::downloadButton
+    shiny::uiOutput
+    shiny::renderUI
+    shiny::verbatimTextOutput
+    shiny::htmlOutput
+    shiny::observeEvent
+    shiny::plotOutput
+    shiny::renderPlot
+
+    # shinyjs
+    shinyjs::useShinyjs
+    shinyjs::disable
+    shinyjs::enable
+
+    # shinyFiles
+    shinyFiles::shinyFileChoose
+
+    # bslib
+    bslib::bs_theme
+    bslib::bs_theme_update
+    bslib::layout_columns
+    bslib::breakpoints
+
+    # htmltools
+    htmltools::HTML
+    htmltools::tags
+
+    # plotly
+    plotly::plot_ly
+    plotly::layout
+
+    # reactable
+    reactable::reactable
+
+    # ggplot2
+    ggplot2::ggplot
+    ggplot2::geom_bar
+    ggplot2::facet_wrap
+    ggplot2::labs
+
+    # dplyr
+    dplyr::filter
+
+    # tidyr
+    tidyr::pivot_wider
+
+    # magrittr
+    magrittr::`%>%`
+    magrittr::set_colnames
+
+    # MLmorph
+    MLmorph::load_data
+    MLmorph::create_rf_model
+    MLmorph::create_morphospace
+    MLmorph::factorize_nicely_dataframe
+    MLmorph::factorize_numeric_vector
+    MLmorph::factorize_binary_vector
+    MLmorph::factorize_character_vector
+    MLmorph::factorize_identity
+
+    return(NULL)
 }
